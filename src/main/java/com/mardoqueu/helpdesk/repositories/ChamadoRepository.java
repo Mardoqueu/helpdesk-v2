661ba59bf5b435703dfd757f1ea37eb8818df91f
Mardoqueu <mdq767@gmail.com>
2022-05-21 16:23:51 -0300

    repositories criados

---
+++ src/main/java/com/mardoqueu/helpdesk/repositories/ChamadoRepository.java
@@ -0,0 +1,10 @@
+package com.mardoqueu.helpdesk.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mardoqueu.helpdesk.domain.Chamado;
+
+public interface ChamadoRepository extends JpaRepository<Chamado, Integer>{
+
+	
+}
---
+++ src/main/java/com/mardoqueu/helpdesk/repositories/PessoaRepository.java
@@ -0,0 +1,10 @@
+package com.mardoqueu.helpdesk.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mardoqueu.helpdesk.domain.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+
+	
+}
