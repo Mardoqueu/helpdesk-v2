0cd63dc4d325733f6cbe2e49dd02f02a04c73c86
Mardoqueu <mdq767@gmail.com>
2022-05-21 22:12:46 -0300

    perfil de desenvolvimento

--- src/main/java/com/mardoqueu/helpdesk/config/DevConfig.java
+++ src/main/java/com/mardoqueu/helpdesk/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.mardoqueu.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,16 +9,22 @@ import org.springframework.context.annotation.Profile;
 import com.mardoqueu.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 		
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@Bean
-	public void instanciaDB() {
-		this.dbService.instanciaDB();
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
 		
+		return false;
 	}
 	
 }
