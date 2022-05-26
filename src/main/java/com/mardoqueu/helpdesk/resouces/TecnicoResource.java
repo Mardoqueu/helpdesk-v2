c4c16532a5cb4eb85bfe4f6427dc9528454fe1dc
Mardoqueu <mdq767@gmail.com>
2022-05-26 15:00:01 -0300

    Padrão DTO para técnico

--- src/main/java/com/mardoqueu/helpdesk/resouces/TecnicoResource.java
+++ src/main/java/com/mardoqueu/helpdesk/resouces/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mardoqueu.helpdesk.domain.Tecnico;
+import com.mardoqueu.helpdesk.domain.dtos.TecnicoDTO;
 import com.mardoqueu.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 		
 	}
 }
