7e013a573357f9feca1841ebbdc719afc42425c2
Mardoqueu <mdq767@gmail.com>
2022-05-27 11:21:40 -0300

    endpoint CREATE para tecnico

--- src/main/java/com/mardoqueu/helpdesk/domain/Tecnico.java
+++ src/main/java/com/mardoqueu/helpdesk/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.mardoqueu.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mardoqueu.helpdesk.domain.dtos.TecnicoDTO;
 import com.mardoqueu.helpdesk.domain.enums.Perfil;
 
 
@@ -30,6 +32,18 @@ public class Tecnico extends Pessoa{
 		addPerfil(Perfil.CLIENTE);
 
 	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+
 
 	public List<Chamado> getChamados() {
 		return chamados;
--- src/main/java/com/mardoqueu/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/mardoqueu/helpdesk/domain/dtos/TecnicoDTO.java
@@ -29,6 +29,7 @@ public class TecnicoDTO implements Serializable{
 
 	public TecnicoDTO() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public TecnicoDTO(Tecnico obj) {
