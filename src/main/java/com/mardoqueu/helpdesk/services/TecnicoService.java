427bffe5d32ac6b12c7e013b86455679e1a9f917
Mardoqueu <mdq767@gmail.com>
2022-05-27 11:42:12 -0300

    exceção para CPF e EḾAIL já cadastrados

--- src/main/java/com/mardoqueu/helpdesk/services/TecnicoService.java
+++ src/main/java/com/mardoqueu/helpdesk/services/TecnicoService.java
@@ -7,9 +7,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mardoqueu.helpdesk.domain.Pessoa;
 import com.mardoqueu.helpdesk.domain.Tecnico;
 import com.mardoqueu.helpdesk.domain.dtos.TecnicoDTO;
+import com.mardoqueu.helpdesk.repositories.PessoaRepository;
 import com.mardoqueu.helpdesk.repositories.TecnicoRepository;
+import com.mardoqueu.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.mardoqueu.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
@@ -17,6 +20,9 @@ public class TecnicoService {
 	
 	@Autowired
 	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -29,7 +35,21 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCfpEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCfpEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema");
+		}
+		
 	}
 }
--- src/main/java/com/mardoqueu/helpdesk/services/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/mardoqueu/helpdesk/services/exceptions/ResourceExceptionHandler.java
@@ -23,4 +23,16 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 		
 	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, 
+			HttpServletRequest request){
+		
+		StandardError error = new StandardError(System.currentTimeMillis(), 
+				HttpStatus.BAD_REQUEST.value(), 
+				"Violação de Dados", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		
+	}
 }
