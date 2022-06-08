b0f5e72aef8028be340810e5d1f648a29104e66d
Mardoqueu <mdq767@gmail.com>
2022-06-08 11:59:28 -0300

    CRUD para cliente implementado

--- src/main/java/com/mardoqueu/helpdesk/domain/Cliente.java
+++ src/main/java/com/mardoqueu/helpdesk/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.mardoqueu.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mardoqueu.helpdesk.domain.dtos.ClienteDTO;
 import com.mardoqueu.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -26,6 +28,17 @@ public class Cliente extends Pessoa{
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
 
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
