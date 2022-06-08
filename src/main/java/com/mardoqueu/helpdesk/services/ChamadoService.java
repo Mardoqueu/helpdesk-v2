65f190ba79e8f5ffbc05e87a9cd541dae8f26dfa
Mardoqueu <mdq767@gmail.com>
2022-06-08 13:13:38 -0300

    findById para chamado

---
+++ src/main/java/com/mardoqueu/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.mardoqueu.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mardoqueu.helpdesk.domain.Chamado;
+import com.mardoqueu.helpdesk.repositories.ChamadoRepository;
+import com.mardoqueu.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+	
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado: ID: " + id));
+	}
+}
