d99cca48ca38fe6f80a5ce37be281df9afb5236a
Mardoqueu <mdq767@gmail.com>
2022-06-09 13:26:41 -0300

    findAll para chamado

--- src/main/java/com/mardoqueu/helpdesk/resouces/ChamadoResource.java
+++ src/main/java/com/mardoqueu/helpdesk/resouces/ChamadoResource.java
@@ -1,5 +1,8 @@
 package com.mardoqueu.helpdesk.resouces;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,5 +25,12 @@ public class ChamadoResource {
 	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll(){
+		List<Chamado> list = service.findById();
+		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 } 
