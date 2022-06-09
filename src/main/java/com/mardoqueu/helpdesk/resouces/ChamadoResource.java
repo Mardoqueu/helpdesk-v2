2d70e506af674bd96ca57083637f851afcabffbb
Mardoqueu <mdq767@gmail.com>
2022-06-09 13:46:40 -0300

    create para chamado

--- src/main/java/com/mardoqueu/helpdesk/resouces/ChamadoResource.java
+++ src/main/java/com/mardoqueu/helpdesk/resouces/ChamadoResource.java
@@ -1,14 +1,20 @@
 package com.mardoqueu.helpdesk.resouces;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mardoqueu.helpdesk.domain.Chamado;
 import com.mardoqueu.helpdesk.domain.dtos.ChamadoDTO;
@@ -32,5 +38,13 @@ public class ChamadoResource {
 		List<Chamado> list = service.findById();
 		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+		Chamado obj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 } 
--- src/main/java/com/mardoqueu/helpdesk/services/ChamadoService.java
+++ src/main/java/com/mardoqueu/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.mardoqueu.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mardoqueu.helpdesk.domain.Chamado;
+import com.mardoqueu.helpdesk.domain.Cliente;
+import com.mardoqueu.helpdesk.domain.Tecnico;
+import com.mardoqueu.helpdesk.domain.dtos.ChamadoDTO;
+import com.mardoqueu.helpdesk.domain.enums.Prioridade;
+import com.mardoqueu.helpdesk.domain.enums.Status;
 import com.mardoqueu.helpdesk.repositories.ChamadoRepository;
 import com.mardoqueu.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -15,6 +22,10 @@ public class ChamadoService {
 	
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteservice;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -23,5 +34,26 @@ public class ChamadoService {
 
 	public List<Chamado> findById() {
 		return repository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {		
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteservice.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 }
